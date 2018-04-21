30285e92fb56655107253b560962dda47f9218f2
Bo Chen <bo.chen@macys.com>
2018-04-21 10:52:11 -0400

    add release year

--- client/src/App.js
+++ client/src/App.js
@@ -9,14 +9,14 @@ class App extends Component {
   componentDidMount() {
     fetch('/movies')
       .then(res => res.json())
-      .then(titles => this.setState({ titles: titles ,  list: titles }));
+      .then(titles => this.setState({ titles: titles, list: titles }));
   }
 
   // base on user input, filerting list
   filerting(event) {
     console.log(event.target.value);
     // real time updae user input
-    this.setState({typed: event.target.value});
+    this.setState({ typed: event.target.value });
     let value = event.target.value;
 
     if (!value.length) {
@@ -50,7 +50,12 @@ class App extends Component {
             <div className="col-12">
               <div className="input-group mb-3">
                 <div className="input-group-prepend">
-                  <span className="input-group-text" id="basic-addon1" role="img"  aria-label="searchIcon">
+                  <span
+                    className="input-group-text"
+                    id="basic-addon1"
+                    role="img"
+                    aria-label="searchIcon"
+                  >
                     🔍
                   </span>
                 </div>
@@ -67,45 +72,67 @@ class App extends Component {
             <div className="col-12">
               <div className="list-group-flush">
                 {this.state.list.map(title => (
-                  <button 
-                    type="button" 
+                  <button
+                    type="button"
                     className="list-group-item list-group-item-action"
                     key={title['_id']}
-                    onClick={() => this.getDetail(title['_id'])}>
+                    onClick={() => this.getDetail(title['_id'])}
+                  >
                     {title['TitleName']}
                   </button>
                 ))}
               </div>
-            </div>       
+            </div>
           </div>
 
           {/* Movie detail info section */}
           <div className="col-lg-8 col-sm-12">
-            <h2 className="text-center">{this.state.detail ? this.state.detail['TitleName'] : ''}</h2>
+            <h2 className="text-center">
+              {this.state.detail ? this.state.detail['TitleName'] : ''}
+              {this.state.detail ? (
+                <span className="text-muted"> - { this.state.detail.ReleaseYear }</span>
+              ) : (
+                ''
+              )}
+            </h2>
             {/* Genres section */}
-            {
-              this.state.detail ?
-              this.state.detail.Genres.map(genre => (
-                <span className="badge badge-dark m-2">{ genre }</span>
-              ))
-              : ''
-            }
+            {this.state.detail
+              ? this.state.detail.Genres.map(genre => (
+                  <span className="badge badge-dark m-2">{genre}</span>
+                ))
+              : ''}
             <div className="row">
-            {/* Awards section */}
-            {
-              this.state.detail ? 
-              this.state.detail.Awards.map(award => (
-                  <div className="col-lg-3 p-0 card border-info mb-3 mr-2" style={ {'maxWidth': '220px'} }>
-                    <div className="card-header">{ award.AwardCompany } { award.AwardWon ? <span className="badge badge-danger">Won</span> : ''}</div>
-                    <div className="card-body text-danger">
-                      <h5 className="card-title font-weight-bold">{ award.Award }</h5>
+              {/* Awards section */}
+              {this.state.detail
+                ? this.state.detail.Awards.map(award => (
+                    <div
+                      className="col-lg-3 p-0 card border-info mb-3 mr-2"
+                      style={{ maxWidth: '220px' }}
+                    >
+                      <div className="card-header">
+                        {award.AwardCompany}{' '}
+                        {award.AwardWon ? (
+                          <span className="badge badge-danger">Won</span>
+                        ) : (
+                          ''
+                        )}
+                      </div>
+                      <div className="card-body text-danger">
+                        <h5 className="card-title font-weight-bold">
+                          {award.Award}
+                        </h5>
+                      </div>
+                      <p className="card-text">
+                        {award.hasOwnProperty('Participants')
+                          ? award.Participants.toString()
+                          : ''}
+                      </p>
+                      <small className="card-text text-muted text-right">
+                        - {award.AwardYear}
+                      </small>
                     </div>
-                    <p className="card-text">{ award.hasOwnProperty('Participants') ? award.Participants.toString() : '' }</p>
-                    <small className="card-text text-muted text-right">- { award.AwardYear }</small>
-                  </div>
-              ))
-              : ''
-            }
+                  ))
+                : ''}
             </div>
           </div>
         </div>
